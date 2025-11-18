5662818fe65f7479f6c8553ec217d8758833d7b6
Josspoot <josspootmateo@gmail.com>
2025-11-18 17:32:04 -0600

    Ejercicio 2.2

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -251,9 +251,20 @@ function ajustarBrillo(matriz, factor) {
  * // Rojo (255,0,0) → Cian (0,255,255)
  */
 function invertirColores(matriz) {
-  // TODO: Implementar inversión de colores
+  const resultado = copiarMatriz(matriz);
 
-  return []; // REEMPLAZAR
+  for (let i = 0; i < resultado.length; i++) {
+    for (let j = 0; j < resultado[i].length; j++) {
+      resultado[i][j] = {
+        r: 255 - matriz[i][j].r,
+        g: 255 - matriz[i][j].g,
+        b: 255 - matriz[i][j].b,
+        a: matriz[i][j].a,
+      };
+    }
+  }
+
+  return resultado;
 }
 
 /**
